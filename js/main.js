6f1bf68c321eb6104fc45558038bd728260bf58e
Glaucio Araujo <glauciousa@gmail.com>
2018-03-06 23:42:53 -0700

    Login Modal mode switch now works

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ function closeLoginModal() {
 	var modal = document.getElementById('loginModal');
 	modal.classList.add('inactive');
 }
+
+function loginWithPrivateKey() {
+	var privateKeyLoginWrapper = document.getElementById('loginWithPrivateKeyWrapper');
+	var publicKeyLoginWrapper = document.getElementById('loginWithPublicKeyWrapper');
+	var privateOptionButton = document.getElementById('privateKeyLoginButton');
+	var publicOptionButton = document.getElementById('publicKeyLoginButton');
+	privateKeyLoginWrapper.classList.remove('inactive');
+	privateOptionButton.classList.add('active');
+	publicKeyLoginWrapper.classList.add('inactive');
+	publicOptionButton.classList.remove('active');
+}
+
+function loginWithPublicKey() {
+	var privateKeyLoginWrapper = document.getElementById('loginWithPrivateKeyWrapper');
+	var publicKeyLoginWrapper = document.getElementById('loginWithPublicKeyWrapper');
+	var privateOptionButton = document.getElementById('privateKeyLoginButton');
+	var publicOptionButton = document.getElementById('publicKeyLoginButton');
+	privateKeyLoginWrapper.classList.add('inactive');
+	privateOptionButton.classList.remove('active');
+	publicKeyLoginWrapper.classList.remove('inactive');
+	publicOptionButton.classList.add('active');
+}
